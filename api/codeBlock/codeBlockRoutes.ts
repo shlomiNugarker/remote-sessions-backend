5e542c99cd619411e7f65bfe772e9e1ce66d988b
Shlomi Nugarker <shlomin1231@gmail.com>
2023-01-16 21:59:19 +0200

    put the validateToken func in the middlewers

--- api/codeBlock/codeBlockRoutes.ts
+++ api/codeBlock/codeBlockRoutes.ts
@@ -1,11 +1,16 @@
 import express from 'express'
 import codeBlockController from './codeBlockController'
+import { jwtService } from '../../services/jwtService'
+
+const { getCodeBlocksIds, getCodeBlock, updateCodeBlock, addCodeBlock } =
+  codeBlockController
+const { validateToken } = jwtService
 
 const router = express.Router()
 
-router.get('/', codeBlockController.getCodeBlocksIds)
-router.get('/:id', codeBlockController.getCodeBlock)
-router.put('/:id', codeBlockController.updateCodeBlock)
-router.post('/', codeBlockController.addCodeBlock)
+router.get('/', getCodeBlocksIds)
+router.get('/:id', validateToken, getCodeBlock)
+router.put('/:id', validateToken, updateCodeBlock)
+router.post('/', validateToken, addCodeBlock)
 
 export default router
